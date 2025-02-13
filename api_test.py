8e5e4182c77d0b42448ee056ed9cfed3f6c7975a
Anish Raj <rajanish4@gmail.com>
2025-02-13 11:08:19 +0100

    Update api_test.py

--- api_test.py
+++ api_test.py
@@ -53,7 +53,7 @@ def main():
     tester.test_generate_endpoint('inputs/images/mri_brain.jpg')
     
     # Test config-based generation
-    tester.test_generate_from_config('inputs/configs/parameters.json')
+    tester.test_generate_from_config('parameters.json')
 
 if __name__ == "__main__":
     main() 
